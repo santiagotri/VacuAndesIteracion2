b461d038616c0d869dea845b94542823b0cc6079
j.ramirezb <j.ramirezb@uniandes.edu.co>
2021-04-08 13:12:51 -0500

    ContraindicacionesSQL listo

--- st-parranderos-jdo-est/src/main/java/uniandes/isis2304/vacuandes/persistencia/SQLListContraindicacionesVacuna.java
+++ st-parranderos-jdo-est/src/main/java/uniandes/isis2304/vacuandes/persistencia/SQLListContraindicacionesVacuna.java
@@ -1,5 +1,8 @@
 package uniandes.isis2304.vacuandes.persistencia;
 
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
 public class SQLListContraindicacionesVacuna {
 	
 	/* ****************************************************************
@@ -31,4 +34,23 @@ public class SQLListContraindicacionesVacuna {
 		this.pp = pp;
 	}
 
+	public long adicionarContraindicacionesVacuna(PersistenceManager pm, long vacuna, String condicion)
+	{
+		Query q = pm.newQuery(SQL, "INSERT INTO " + pp.darTablaListCondicionesCiudadano () + "(vacuna, condicion) values (?, ?)");
+		q.setParameters(vacuna, condicion);
+		return (long) q.executeUnique(); 
+	}
+	
+	public long eliminarTodasLasContraindicacionesVacuna(PersistenceManager pm)
+	{
+		Query q = pm.newQuery(SQL, "DELETE FROM " + pp.darTablaListContraindicacionesVacuna());
+		return (long) q.executeUnique();
+	}
+	
+	public long eliminarContraindicacionesVacuna(PersistenceManager pm, long vacuna, String condicion)
+	{
+		Query q = pm.newQuery(SQL, "DELETE FROM " + pp.darTablaListContraindicacionesVacuna() + " WHERE vacuna = ? AND condicion = ?");
+        q.setParameters(vacuna, condicion);
+        return (long) q.executeUnique();
+	}
 }
